e3ce095c7b2b80c7db9945764f159fcafb8c6460
Daniel Nestor <63206357+danielndev@users.noreply.github.com>
2020-10-14 00:02:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const flash = require('connect-flash');
 
 const ShortURL = require('./model/ShortURL');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const URL = "url.io/";
 
